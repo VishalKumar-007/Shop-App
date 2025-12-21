eaf827b574e37ee63c11ce82a9f46a87635d7824
Vishal Kumar <vishalofficialdev@gmail.com>
2025-12-21 22:09:12 +0530

    added bottom Navigation Bar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -14,6 +14,8 @@ class _HomePageState extends State<HomePage> {
   final List<String> filters = const ['All', 'Adidas', 'Nike', 'Bata'];
   late String selectedFilter;
 
+  int currentPage = 0;
+
   @override
   void initState() {
     super.initState();
@@ -139,6 +141,24 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) {
+          setState(() {
+            currentPage = value;
+          });
+        },
+        currentIndex: currentPage,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: '',
+          ),
+        ],
       ),
     );
   }
