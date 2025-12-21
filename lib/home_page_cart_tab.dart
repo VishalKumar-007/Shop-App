e590f7f20d7c2dc8e2a392551071d654f917c9ee
Vishal Kumar <vishalofficialdev@gmail.com>
2025-12-22 00:48:07 +0530

    added cartPage UI

--- lib/home_page_cart_tab.dart
+++ lib/home_page_cart_tab.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/global_variables.dart';
 
 class HomePageCartTab extends StatelessWidget {
   const HomePageCartTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Cart Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cart'),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: cart.length,
+        itemBuilder: (context, index) {
+          final cartItem = cart[index];
+
+          // ListTile widget used to create a list of tiles like UI
+          return ListTile(
+            // CircleAvatar widget provides a rounded border image container
+            // where we can pass background Image
+            leading: CircleAvatar(
+              backgroundImage: AssetImage(cartItem['imageUrl'] as String),
+              radius: 30,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.red,
+              ),
+            ),
+            title: Text(
+              cartItem['title'].toString(),
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            subtitle: Text('Size: ${cartItem['sizes']}'),
+          );
+        },
       ),
     );
   }
